b5ddb155c435a084cbc777c2f0f064feb76f1671
FtKuBo <semmarmehdi558@gmail.com>
2024-01-16 15:13:53 -0500

    cleaning

--- BigIntCalculator.java
+++ BigIntCalculator.java
@@ -1,37 +1,38 @@
+// except using the BigInt library we cannot add extremly big numbers
+
+import java.math.BigInteger;
+
 public class BigIntCalculator {
-    public static String add(String a, String b) {  // fait la somme de deux nombre trés grand pas le droit à BigInt
+    public static String add(String a, String b) {   //Sum two very large numbers without using BigInt.
 
 
-        Integer reste = 0 ;                                 // on va faire la somme comme au primaire avec le reste
-        String res = "" ;                                   // on va ensuite ajouter petit à petit le résultat                                    
+        Integer reste = 0 ;                                 // oui will do the sum method with carry
+        String res = "" ;                                                                     
         String nbZero = "";
-      
         int longueurDiff = a.length() - b.length();
-        if (longueurDiff < 0) {longueurDiff*=-1;}
+
+        if (longueurDiff < 0) { longueurDiff*=-1; }
  
         for (int num = 0 ; num<longueurDiff ; num++ ) {nbZero += "0" ;}
         
-        if (a.length()>b.length()){
-            b = nbZero + b;
-        }
-        if (b.length()>a.length()){
-            a = nbZero + a;
-        }
+        if (a.length()>b.length()){ b = nbZero + b; }
+
+        if (b.length()>a.length()){ a = nbZero + a; }
 
         for(int i = a.length()-1 ; i>=0 ; i-- ){
 
-            String miniSomme = String.                      // on fait la somme des elements à l'index i
+            String miniSomme = String.                      // we will do the sum of the numbers at the index i
                 valueOf(Integer.valueOf(a.substring(i,i+1))+Integer.valueOf(b.substring(i,i+1))+reste);
           
 
             if(miniSomme.length()>1)
-            reste = Integer.                                // on récupére le reste soit tout les chiffres situé donc aprés le premier donc quand la somme > 10
+            reste = Integer.                                // we store the carry of the sum of the two digits in reste
                 valueOf(miniSomme.substring(0, miniSomme.length()-1));
             else
             reste = 0 ;
           
 
-            res = miniSomme.                                // on ajoute l'unité de la somme à notre minisomme
+            res = miniSomme.                                // we add the unit of the sum to our value
             substring(miniSomme.length()-1,miniSomme.length())+res;
             
             
@@ -39,16 +40,16 @@ public class BigIntCalculator {
         
         if (reste > 0) {res = String.valueOf(reste) + res;}
         
-        res .replaceFirst("^0+","");     // on enléve les zéros au début du avant le nombre
-
-        System.out.println(a + " + "+ b +" = "+res);
+        res.replaceFirst("^0+","");     // we remove zeros at the lefthand side of the number
 
         return res;
     }
     
     public static void main(String[] args){
         try{
-        add("1893834934934930", "1734834734939934801");
+            String Value1 = "1893834934934930";
+            String Value2 = "1734834734939934801";
+            System.out.println(Value1+" + "+Value2+" = "+add(Value1, Value2));
         }
         catch(Exception e){
             System.out.println("not working : "+ e);
